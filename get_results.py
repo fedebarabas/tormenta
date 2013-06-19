614f41e5aab03dd096d33672431f3b41989923a1
Federico Barabas <fede.barabas@gmail.com>
2013-06-19 20:58:53 -0300

    export png results

--- get_results.py
+++ get_results.py
@@ -1,8 +1,11 @@
 import library.readinsight3 as readinsight3
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from matplotlib import rc
 import numpy as np
+import scipy.ndimage
+import matplotlib.cm as cm
 
 def find_nearest(array,value):
         idx = (np.abs(array-value)).argmin()
@@ -21,47 +24,45 @@ x_locs = i3_data_in['xc']*px_size                 # px -> um conversion
 y_locs = i3_data_in['yc']*px_size
 # campoi = i3_data_in['i']
 
-# plt.plot(0.001*x_locs, 0.001*y_locs, 'b', marker='o', markersize=0.5, linestyle='None')
-# # plt.scatter(x_locs, y_locs,s=sigma)
-# plt.xlim((0,0.001*shape[0]))
-# plt.ylim((0,0.001*shape[1]))
-# plt.xlabel('$\mu$m')
-# plt.ylabel('$\mu$m')
-# # plt.show()
-# # p rint(campoi)
-
-# imagen = np.zeros([np.ceil(shape[0]/render_px), np.ceil(shape[1]/render_px)])     # I need to add px_size to each definition due to indexing
-# # pimagen = np.zeros([(shape[0]+px_size)/render_px, (shape[1]+px_size)/render_px])     # I need to add px_size to each definition due to indexing
-# # rint 'imagen_px ', shape[0]/render_px, shape[1]/render_px
-# for i in np.arange(len(x_locs)):
-#     print x_locs[i], np.trunc(x_locs[i]/render_px) 
-#     imagen[np.trunc(x_locs[i]/render_px),np.trunc(y_locs[i]/render_px)] += 1    # I need to divide by render_px to place the point in the final image
-# 
-# plt.imshow(imagen,cmap='gray',interpolation='none')
-# plt.colorbar()
-# plt.show()
-
 H, xedges, yedges = np.histogram2d(0.001*y_locs, 0.001*x_locs, bins=np.ceil(shape/render_px))
 
-punto1 = find_nearest(xedges,3.1)[1],find_nearest(yedges,12.6)[1]
-punto2 = find_nearest(xedges,4.6)[1],find_nearest(yedges,9.8)[1]
+# Image saving 
+scipy.misc.imsave('out.png', H)
+
+
+punto1 = find_nearest(xedges,4.5)[1],find_nearest(yedges,10)[1]
+punto2 = find_nearest(xedges,5.2)[1],find_nearest(yedges,8.6)[1]
 print punto1, punto2
 print H[punto1], H[punto2]
 length = int(np.hypot(punto2[0]-punto1[0], punto2[1]-punto1[1]))
+length = 1000000
 print length
 lin_x, lin_y = np.linspace(punto1[0], punto2[0], length), np.linspace(punto1[1], punto2[1], length)
 profile = H[lin_x.astype(np.int), lin_y.astype(np.int)]
+# profile = scipy.ndimage.map_coordinates(H, np.vstack((lin_x,lin_y)))
 
 extent = [yedges[0], yedges[-1],  xedges[0], xedges[-1]]
 
-# fig, axes = plt.subplots(1,2)
-# axes[0].imshow(H, extent=extent, cmap='gray', vmax = 10, interpolation='none')
-# # axes[0].plot([punto1[0], punto2[0]], [punto1[1], punto2[1]], 'ro-')
-# axes[0].plot([3.1, 4.6], [12.6, 9.8], 'ro-')
-# axes[0].axis('image')
-# axes[1].plot(profile)
+# dpi = 180
+# margin = 0.05 # (5% of the width/height of the figure...)
+# figsize = (1 + margin) * np.ceil(shape/render_px) / dpi
+# figsize = (1 + margin)*H.shape[1]/dpi, (1 + margin)*H.shape[0]/dpi
+print  H.shape[1], H.shape[0]
+fig = plt.figure(figsize=(8.0, 10.0))
+# fig = plt.figure(figsize=figsize, dpi=dpi)
+gs = gridspec.GridSpec(2, 1,height_ratios=[2,1])
 
-plt.imshow(H, extent=extent, cmap='gray', vmax = 10, interpolation='none')
+ax1 = plt.subplot(gs[0])
+img = ax1.imshow(H, extent=extent, cmap='gray', vmax = 10, interpolation='none')
+plt.xlabel('$\mu$m')
+plt.ylabel('$\mu$m')
+print fig.get_size_inches()
+fig.savefig('ax2_figure.png',  bbox_inches='tight')
+fig.colorbar(img, ax=ax1)
 # axes[0].plot([punto1[0], punto2[0]], [punto1[1], punto2[1]], 'ro-')
-# axes[0].plot([3.1, 4.6], [12.6, 9.8], 'ro-')
+ax1.plot([4.5, 5.2], [10, 8.6], 'ro-')
+ax1.axis('image')
+
+ax2 = plt.subplot(gs[1])
+ax2.plot(profile)
 plt.show()
