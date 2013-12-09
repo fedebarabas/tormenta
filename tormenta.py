138789957eaca5dc9d18e76bfeccfe6a632321db
Federico Barabas <barabas@df.uba.ar>
2013-12-09 18:23:10 +0100

    gggg

--- tormenta.py
+++ tormenta.py
@@ -6,8 +6,13 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-import tkFileDialog as filedialog
-from Tkinter import Tk
+# Python2-3 compatiblilty handling
+try:
+    from tkinter import Tk, filedialog
+
+except ImportError:
+    import tkFileDialog as filedialog
+    from Tkinter import Tk
 
 def openraw(filename, shape=None, datatype=np.dtype('uint16')):
     # 16-bit unsigned little-endian byte order
@@ -15,7 +20,7 @@ def openraw(filename, shape=None, datatype=np.dtype('uint16')):
     fileobj = open(filename, 'rb')
 
     if shape==None:
-        
+
         print('Shape not provided, loading it from inf file')
         rootname, ext = os.path.splitext(filename)
         inf_name = rootname + '.inf'
@@ -28,7 +33,7 @@ def openraw(filename, shape=None, datatype=np.dtype('uint16')):
         print(shape)
 
     data = np.fromfile(fileobj, dtype=datatype).byteswap().reshape(shape)
-    
+
     return data, shape
 
 def show_frame(frame):
@@ -38,73 +43,72 @@ def show_frame(frame):
     plt.show()
 
 class Stack():
-    
+
     def __init__(self, filename=None):
-        
+
         if filename==None:
             # File dialog
             root = Tk()
-            filename = filedialog.askopenfilename(parent=root, 
+            filename = filedialog.askopenfilename(parent=root,
                                                   initialdir=os.getcwd())
             root.destroy()
 
         self.filename = filename
         print(self.filename)
         os.chdir(os.path.split(self.filename)[0])
-        
+
         rootname, self.ext = os.path.splitext(self.filename)
-        
+
         if self.ext in ['.tiff', '.tif']:
             self.image = Image.open(self.filename)
             self.size = self.image.size
-            
+
         else:
             print("Loading as raw image file")
             self.image, self.size = openraw(self.filename)
 
-#        try:    
+#        try:
 #            while 1:
 #                self.image.seek(self.image.tell() + 1)
-#                
+#
 #        except EOFError:
 #            pass
-#            
+#
 #        self.nframes = self.image.tell()
-        
-        
-        
+
+
+
 
     def frame(self, n):
-        
+
         if self.ext in ['.tiff', '.tif']:
 
             try:
                 self.image.seek(n)
                 data = np.array(self.image.getdata())
-                
+
                 return np.transpose(np.reshape(data, (self.size[0], self.size[1])))
-            
+
             except EOFError:
                 print(n, "is greater than the number of frames of the stack")
-            
+
         else:
             return self.image[n]
-            
-            
+
+
 if __name__ == "__main__":
-    
+
 #    %load_ext autoreload
 #    %autoreload 2
-    
+
     data = Stack()
 #    print(data.frame(2))
-    
+
 #    show_frame(data.frame(20))
-    
+
 #    mean = np.array([data.frame(i).mean() for i in np.arange(10, 1000)])
 #    print(mean.mean())
 #    print(mean.std())
-    
-    
-    
-    
+
+
+
--- tormenta_app.py
+++ tormenta_app.py
@@ -7,9 +7,7 @@ Created on Sun Dec  8 22:47:36 2013
 
 from __future__ import division, with_statement, print_function
 
-import Image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
@@ -24,8 +22,8 @@ def openraw(filename, shape=None, datatype=np.dtype('uint16')):
 
     fileobj = open(filename, 'rb')
 
-    if shape==None:
-        
+    if shape is None:
+
         print('Shape not provided, loading it from inf file')
         rootname, ext = os.path.splitext(filename)
         inf_name = rootname + '.inf'
@@ -38,91 +36,123 @@ def openraw(filename, shape=None, datatype=np.dtype('uint16')):
         print(shape)
 
     data = np.fromfile(fileobj, dtype=datatype).byteswap().reshape(shape)
-    
+
     return data, shape
 
-class Stack():
-    
+
+class Stack(object):
+
     def __init__(self, filename):
 
         self.image = Image.open(filename)
         self.size = self.image.size
-        
-        try:    
-            while 1:
-                self.image.seek(self.image.tell() + 1)
-                
-        except EOFError:
-            pass
-            
-        self.nframes = self.image.tell()
-        
+        self.nframes = None
+        self.frame = 0
 
-    def frame(self, n):
-        
-        if n <= self.nframes:
-            self.image.seek(n)
-            data = np.array(self.image.getdata()).reshape(self.image.size[::-1])
-            return np.transpose(data)
-        
-        else:
-            print(n, "is greater than the number of frames of the stack")
+    @property
+    def frame(self):
+        return self.image.tell()
 
-app = QtGui.QApplication([])
-win = QtGui.QMainWindow()
-area = DockArea()
-win.setCentralWidget(area)
-win.resize(1200, 700)
-win.setWindowTitle('pyqtgraph example: dockarea')
+    @frame.setter
+    def frame(self, value):
 
-## Create docks, place them into the window one at a time.
-## Note that size arguments are only a suggestion; docks will still have to
-## fill the entire dock area and obey the limits of their internal widgets.
-d2 = Dock("Dock2 - Console", size=(500, 300))
-d3 = Dock("Dock3", size=(500, 400))
-d4 = Dock("Dock4 (tabbed) - Plot", size=(500, 200))
-d5 = Dock("Dock5 - Image", size=(700, 500))
-d6 = Dock("Dock6 (tabbed) - Plot", size=(500, 200))
-area.addDock(d2, 'right')     
-area.addDock(d3, 'top', d2)      
-area.addDock(d4, 'above', d3)     
-area.addDock(d5, 'left')  
-area.addDock(d6, 'above', d3)
-    
-## Test ability to move docks programatically after they have been placed
-#area.moveDock(d4, 'top', d2)     ## move d4 to top edge of d2
-#area.moveDock(d6, 'above', d4)   ## move d6 to stack on top of d4
-#area.moveDock(d5, 'top', d2)     ## move d5 to top edge of d2
+        if self.nframes is None:
+            try:
+                self.image.seek(value)
+
+            except EOFError:
+                print(n, "is greater than the number of frames of the stack")
+
+    def data(self, frame=None):
+
+        if frame is not(None):
+            self.frame = frame
+
+        data = np.array(self.image.getdata())
+        data = data.reshape(self.image.size[::-1])
+        return np.transpose(data)
+
+#
+#    def goto_frame(self, k):
+#
+#        n = self.image.tell()
 
 
-## Add widgets into each dock
+def ch_frame(stack, n, widget):
+    frame = stack.frame
+    stack.frame = frame + n
+    widget.setImage(stack.data(frame + n))
 
-w2 = pg.console.ConsoleWidget()
-d2.addWidget(w2)
 
-## Hide title bar on dock 3
-w3 = pg.PlotWidget(title="Plot inside dock with no title bar")
-w3.plot(np.random.normal(size=100))
-d3.addWidget(w3)
+class TormentaGui(QtGui.QMainWindow):
 
-w4 = pg.PlotWidget(title="Dock 4 plot")
-w4.plot(np.random.normal(size=100))
-d4.addWidget(w4)
+    def __init__(self, *args, **kwargs):
 
-data = Stack('muestra.tif')
+        super(QtGui.QMainWindow, self).__init__(*args, **kwargs)
 
-w5 = pg.ImageView()
-#w5.setImage(data.frame(2))
-d5.addWidget(w5)
+        exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(QtGui.qApp.quit)
 
-w6 = pg.PlotWidget(title="Dock 6 plot")
-w6.plot(np.random.normal(size=100))
-d6.addWidget(w6)
-    
-win.show()
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        area = DockArea()
+        self.setCentralWidget(area)
+        self.resize(1200, 700)
+        self.setWindowTitle('Tormenta')
+
+        ## Create docks, place them into the window one at a time.
+        ## Note that size arguments are only a suggestion; docks will still
+        ## have to fill the entire dock area and obey the limits of their
+        ## internal widgets.
+        d2 = Dock("Dock2 - Console", size=(500, 300))
+        d3 = Dock("Dock3", size=(500, 400))
+        d4 = Dock("Dock4 (tabbed) - Plot", size=(500, 200))
+        d5 = Dock("Dock5 - Image", size=(700, 500))
+        d6 = Dock("Dock6 (tabbed) - Plot", size=(500, 200))
+        area.addDock(d2, 'right')
+        area.addDock(d3, 'top', d2)
+        area.addDock(d4, 'above', d3)
+        area.addDock(d5, 'left')
+        area.addDock(d6, 'above', d3)
+
+        ## Add widgets into each dock
+        w2 = pg.console.ConsoleWidget()
+        d2.addWidget(w2)
+
+        ## Hide title bar on dock 3
+        w3 = pg.PlotWidget(title="Plot inside dock with no title bar")
+        w3.plot(np.random.normal(size=100))
+        d3.addWidget(w3)
+
+        w4 = pg.PlotWidget(title="Dock 4 plot")
+        w4.plot(np.random.normal(size=100))
+        d4.addWidget(w4)
+
+        stack = Stack('muestra.tif')
+
+        w5 = pg.ImageView()
+        w5.setImage(stack.data(50))
+        d5.addWidget(w5)
+
+        next_frame = QtGui.QAction('&Go to next frame', self)
+        next_frame.setShortcut('right arrow')
+        next_frame.triggered.connect(ch_frame(stack, 1, w5))
+
+        w6 = pg.PlotWidget(title="Dock 6 plot")
+        w6.plot(np.random.normal(size=100))
+        d6.addWidget(w6)
+
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
-    import sys
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+
+    app = QtGui.QApplication([])
+
+    win = TormentaGui()
+    win.show()
+
+    exit(app.exec_())
