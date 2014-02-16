0c0c3c3519bfac5bc78749af0cdd6c16281b76a3
Federico Barabas <fede.barabas@gmail.com>
2014-02-16 01:57:13 +0100

    Trying to implement Newton as minimizing algorithm

--- stack.py
+++ stack.py
@@ -69,12 +69,124 @@ def get_mode(array):
     return (bin_edges[hist_max + 1] + bin_edges[hist_max]) / 2
 
 
+def logll(params, *args):
+
+    A, x0, y0, bkg = params
+    pico, F = args
+
+    x, y = np.arange(pico.shape[0]), np.arange(pico.shape[1])
+
+    erfi = erf((x + 1 - x0) / F) - erf((x - x0) / F)
+    erfj = erf((y + 1 - y0) / F) - erf((y - y0) / F)
+
+    lambda_p = A * F**2 * np.pi * erfi[:, np.newaxis] * erfj / 4 + bkg
+
+    return - np.sum(pico * np.log(lambda_p) - lambda_p)
+
+
+def ll_jac(params, *args):
+
+    A, x0, y0, bkg = params
+    pico, F = args
+
+    x, y = np.arange(pico.shape[0]), np.arange(pico.shape[1])
+
+    erfi = erf((x + 1 - x0) / F) - erf((x - x0) / F)
+    erfj = erf((y + 1 - y0) / F) - erf((y - y0) / F)
+    expi = np.exp(-(x - x0 + 1)**2/F**2) - np.exp(-(x - x0)**2/F**2)
+    expj = np.exp(-(y - y0 + 1)**2/F**2) - np.exp(-(y - y0)**2/F**2)
+
+    jac = np.zeros(4)
+
+    # Some derivatives made with sympy
+
+    # expr.diff(A)
+    jac[0] = np.sum((np.pi/4)*F**2*pico*erfi[:, np.newaxis] * erfj/((np.pi/4)*A*F**2*erfi[:, np.newaxis] * erfj + bkg) - (np.pi/4)*F**2*erfi[:, np.newaxis] * erfj)
+
+    jac[1] = np.sum(- 0.5 * A * F * np.sqrt(np.pi) * expi[:, np.newaxis] * erfj)
+
+    jac[2] = np.sum(- 0.5 * A * F * np.sqrt(np.pi) * erfi[:, np.newaxis] * expj)
+
+    # expr.diff(y0)
+    jac[3] = np.sum(pico/((np.pi/4)*A*F**2*erfi[:, np.newaxis] * erfj + bkg) - 1)
+
+    return jac
+
+def ll_hess(params, *args):
+
+    A, x0, y0, bkg = params
+    pico, F = args
+
+    x, y = np.arange(pico.shape[0]), np.arange(pico.shape[1])
+
+    erfi = erf((x + 1 - x0) / F) - erf((x - x0) / F)
+    erfj = erf((y + 1 - y0) / F) - erf((y - y0) / F)
+    expi = np.exp(-(x - x0 + 1)**2/F**2) - np.exp(-(x - x0)**2/F**2)
+    expj = np.exp(-(y - y0 + 1)**2/F**2) - np.exp(-(y - y0)**2/F**2)
+
+    hess = np.zeros((4, 4))
+
+    # All derivatives made with sympy
+
+    # expr.diff(A, A)
+    hess[0, 0] = - np.sum(0.616850275068085 * F**4 * pico * erfi**2 * erfj**2 /
+                          ((np.pi/4) * A * F**2 * (erfi * erfj + bkg)**2))
+
+    # expr.diff(A, x0)
+    hess[0, 1] = np.sum(F*(np.exp(-(x - x0 + 1)**2/F**2) - np.exp(-(x - x0)**2/F**2))*(erf((y - y0)/F) - erf((y - y0 + 1)/F))*(-1.23370055013617*A*F**2*pico*(erf((x - x0)/F) - erf((x - x0 + 1)/F))*(erf((y - y0)/F) - erf((y - y0 + 1)/F))/((np.pi/4)*A*F**2*(erf((x - x0)/F) - erf((x - x0 + 1)/F))*(erf((y - y0)/F) - erf((y - y0 + 1)/F)) + bkg)**2 + 1.5707963267949*pico/((np.pi/4)*A*F**2*(erf((x - x0)/F) - erf((x - x0 + 1)/F))*(erf((y - y0)/F) - erf((y - y0 + 1)/F)) + bkg) - 1.5707963267949)/np.sqrt(np.pi))
+    hess[1, 0] = hess[0, 1]
+
+    # expr.diff(A, y0)
+    hess[0, 2] = np.sum(F*(expj)*(-erfi)*(-1.23370055013617*A*F**2*pico*(-erfi)*(-erfj)/((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2 + (np.pi/2)*pico/((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg) - (np.pi/2))/np.sqrt(np.pi))
+    hess[2, 0] = hess[0, 2]
+
+    # expr.diff(A, bkg)
+    hess[0, 3] = np.sum(-(np.pi/4)*F**2*pico*(-erfi)*(-erfj)/((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2)
+    hess[3, 0] = hess[0, 3]
+
+    # expr.diff(x0, x0)
+    hess[1, 1] = np.sum(A*(-erfj)*(-2.46740110027234*A*F**2*pico*(expi)**2*(-erfj)/(np.pi*((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2) - np.pi*pico*((x - x0)*np.exp(-(x - x0)**2/F**2) - (x - x0 + 1)*np.exp(-(x - x0 + 1)**2/F**2))/(np.sqrt(np.pi)*F*((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)) + (np.pi*(x - x0)*np.exp(-(x - x0)**2/F**2) - np.pi*(x - x0 + 1)*np.exp(-(x - x0 + 1)**2/F**2))/(np.sqrt(np.pi)*F)))
+
+    # expr.diff(x0, y0)
+    hess[1, 2] = np.sum(A*(expi)*(expj)*(-2.46740110027234*A*F**2*pico*(-erfi)*(-erfj)/((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2 + np.pi*pico/((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg) - np.pi)/np.pi)
+    hess[2, 1] = hess[1, 2]
+
+    # expr.diff(x0, bkg)
+    hess[1, 3] = np.sum(-(np.pi/2)*A*F*pico*(expi)*(-erfj)/(np.sqrt(np.pi)*((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2))
+    hess[3, 1] = hess[1, 3]
+
+    # expr.diff(y0, y0)
+    hess[2, 2] = np.sum(A*(-erfi)*(-2.46740110027234*A*F**2*pico*(expj)**2*(-erfi)/(np.pi*((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2) - np.pi*pico*((y - y0)*np.exp(-(y - y0)**2/F**2) - (y - y0 + 1)*np.exp(-(y - y0 + 1)**2/F**2))/(np.sqrt(np.pi)*F*((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)) + (np.pi*(y - y0)*np.exp(-(y - y0)**2/F**2) - np.pi*(y - y0 + 1)*np.exp(-(y - y0 + 1)**2/F**2))/(np.sqrt(np.pi)*F)))
+
+    # expr.diff(y0, bkg)
+    hess[2, 3] = np.sum(-(np.pi/2)*A*F*pico*(expj)*(-erfi)/(np.sqrt(np.pi)*((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2))
+    hess[3, 2] = hess[2, 3]
+
+    # expr.diff(bkg, bkg)
+    hess[3, 3] = np.sum(-pico/((np.pi/4)*A*F**2*(-erfi)*(-erfj) + bkg)**2)
+
+    return hess
+
+def fit_peak(peak, fwhm, bkg):
+
+    # First guess of parameters
+    F = fwhm / (2 * np.sqrt(np.log(2)))
+    A = (peak[np.floor(peak.shape[0]/2),
+              np.floor(peak.shape[1]/2)] - bkg) / 0.65
+    x0, y0 = center_of_mass(peak)
+
+    return minimize(logll, x0=[A, x0, y0, bkg], args=(peak, F),
+                    method='Newton-CG', jac=ll_jac, hess=ll_hess)
+#    return minimize(logll, x0=[A, x0, y0, bkg], args=(peak, F),
+#                    method='Powell')
+
 class Peaks(object):
 
     def __init__(self, image, fwhm):
 
         self.image = image
         self.fwhm = fwhm
+        self.size = np.ceil(self.fwhm)
 
     def find(self, kernel, xkernel, alpha=3, size=2):
         """Peak finding routine.
@@ -162,12 +274,12 @@ class Peaks(object):
             p = tuple(peaks[i])
 
             # Sharpness
-            masked = np.ma.masked_array(peak(self.image, p, size), mask)
-            sharpness[i] = image[p] / (image_conv[p] * masked.mean())
+            masked = np.ma.masked_array(self.get_peak(p), mask)
+            sharpness[i] = self.image[p] / (image_conv[p] * masked.mean())
 
             # Roundness
-            hx = np.dot(peak(self.image, p, size)[2, :], xkernel)
-            hy = np.dot(peak(self.image, p, size)[:, 2], xkernel)
+            hx = np.dot(self.get_peak(p)[2, :], xkernel)
+            hy = np.dot(self.get_peak(p)[:, 2], xkernel)
             roundness[i] = 2 * (hy - hx) / (hy + hx)
 
             # Brightness
@@ -180,50 +292,31 @@ class Peaks(object):
         self.roundness = roundness
         self.brightness = brightness
 
+    def get_peak(self, peak):
+        """Caller for the area around the peak."""
+
+        return self.image[peak[0] - self.size:peak[0] + self.size + 1,
+                          peak[1] - self.size:peak[1] + self.size + 1]
+
     def fit(self, n_param=4):
 
-        self.results = np.zeros((len(self.positions), n_param))
+        self.results = np.zeros((len(self.positions), n_param + 3))
 
         for i in np.arange(len(self.positions)):
 
-            peak_arr = peak(self.image, self.positions[i], self.size)
+            peak_arr = self.get_peak(self.positions[i])
             res = fit_peak(peak_arr, self.fwhm, self.bkg)
-            self.results[i] = res.x
+            self.results[i, 0:n_param] = res.x
 
-            ### TODO: CHANGE TO IMAGE COORDINATES
+            # Change fit results to image coordinates
+            self.results[i, 1:3] = (self.results[i, 1:3] - self.size - 0.5
+                                    + self.positions[i])
 
+        self.results[:, n_param] = self.sharpness
+        self.results[:, n_param + 1] = self.roundness
+        self.results[:, n_param + 2] = self.brightness
 
-def peak(img, p, size):
-    """Caller for the area around the peak."""
-
-    return img[p[0] - size:p[0] + size + 1, p[1] - size:p[1] + size + 1]
-
-
-def logll(params, *args):
-
-    A, x0, y0, bkg = params
-    pico, F = args
-
-    x, y = np.arange(pico.shape[0]), np.arange(pico.shape[1])
-
-    erfi = erf((x + 1 - x0) / F) - erf((x - x0) / F)
-    erfj = erf((y + 1 - y0) / F) - erf((y - y0) / F)
-
-    lambda_p = A * F**2 * np.pi * erfi[:, np.newaxis] * erfj / 4 + bkg
-
-    return - np.sum(pico * np.log(lambda_p) - lambda_p)
-
-
-def fit_peak(peak, fwhm, bkg=np.min(peak)):
-
-    # First guess of parameters
-    F = fwhm / (2 * np.sqrt(np.log(2)))
-    A = (peak[np.floor(peak.shape[0]/2),
-              np.floor(peak.shape[1]/2)] - bkg) / 0.65
-    x0, y0 = center_of_mass(peak)
-
-    return minimize(logll, x0=[A, x0, y0, bkg], args=(peak, F),
-                    method='Powell')
+### TODO: Constrains for the fit A>0
 
 
 class Stack(object):
@@ -253,24 +346,51 @@ class Stack(object):
 
         self.frame = 0
         self.fwhm = fwhm(self.lambda_em, self.NA) / self.nm_per_px
+        self.win_size = np.ceil(self.fwhm)
+
         self.kernel = kernel(self.fwhm)
         self.xkernel = xkernel(self.fwhm)
 
+    def find_molecules(self, init=0, end=None, n_param=4):
+
+        if end is None:
+            end = self.nframes
+
+        # I create a big array, I'll keep the non-null part at the end
+        nframes = end - init
+        results = np.zeros((nframes * np.prod(self.size) /
+                           (self.win_size + 1), n_param + 3))
+        index = 0
+
+        for frame in np.arange(init, end):
+
+            peaks = Peaks(stack.image[frame], stack.fwhm)
+            peaks.find(stack.kernel, stack.xkernel)
+            peaks.fit(n_param)
+
+            results[index:index + len(peaks.results)] = peaks.results
+            index = index + len(peaks.results)
+
+            print(100 * (frame - init) / nframes, '% done')
+
+        self.molecules = results[0:index]
 
 if __name__ == "__main__":
 
-    import matplotlib.pyplot as plt
+#    import matplotlib.pyplot as plt
 
     stack = Stack()
     peaks = Peaks(stack.image[10], stack.fwhm)
     peaks.find(stack.kernel, stack.xkernel)
-    plt.imshow(peaks.image, interpolation='nearest')
-    plt.colorbar()
-    plt.plot(peaks.positions[:, 1], peaks.positions[:, 0],
-             'ro', markersize=10, alpha=0.5)
+#    plt.imshow(peaks.image, interpolation='nearest')
+#    plt.colorbar()
+#    plt.plot(peaks.positions[:, 1], peaks.positions[:, 0],
+#             'ro', markersize=10, alpha=0.5)
+#
+##    image = stack.image[10]
+##    pico = peak(image, peaks.positions[10], 2)
+#
+#    peaks.fit()
+#    print(peaks.results)
 
-#    image = stack.image[10]
-#    pico = peak(image, peaks.positions[10], 2)
-
-    peaks.fit()
-    print(peaks.results)
+#    stack.find_molecules(end=100)
