e7cfb6d046af3cbf2334ce29013782bf80738e68
Federico Barabas <fede.barabas@gmail.com>
2014-07-14 18:53:54 -0300

    debug

--- analisis/xydrift.py
+++ analisis/xydrift.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.signal import fftconvolve
+from scipy.ndimage import center_of_mass
 import scipy.optimize as opt
 
 # with open("d1.raw", 'rb') as d1:
@@ -18,37 +19,6 @@ import scipy.optimize as opt
 #        # show()
 
 
-# Generic gaussian definition taken from
-# http://en.wikipedia.org/wiki/Gaussian_function
-def generic_gaussian(xy, bkg, amp, x0, y0, sx, sy, theta):
-
-    x, y = xy
-
-    x0 = float(x0)
-    y0 = float(y0)
-
-    a = np.cos(theta)**2 / (2 * sx**2) + np.sin(theta)**2 / (2 * sy**2)
-    b = - np.sin(2 * theta) / (4 * sx**2) + np.sin(2 * theta) / (4 * sy**2)
-    c = np.sin(theta)**2 / (2*sx**2) + np.cos(theta)**2 / (2 * sy**2)
-
-    g = bkg + amp * np.exp(- (a * (x - x0)**2 +
-                           2 * b * (x - x0) * (y - y0) +
-                           c * (y - y0)**2))
-    return g.ravel()
-
-
-def simmetric_gaussian(xy, bkg, amp, x0, y0, s):
-
-    x, y = xy
-
-    x0 = float(x0)
-    y0 = float(y0)
-
-    g = bkg + amp * np.exp(-(((x0 - x)/s)**2 + ((y0 - y)/s)**2) * 2)
-
-    return g.ravel()
-
-
 def raw_moment(data, iord, jord):
     nrows, ncols = data.shape
     y, x = np.mgrid[:nrows, :ncols]
@@ -56,7 +26,12 @@ def raw_moment(data, iord, jord):
     return data.sum()
 
 
-def parameters(data):
+def gen_gauss_est(data):
+
+    height = np.max(data) - np.median(data)
+    bkg = np.median(data)
+
+    data = data - np.min(data)
 
     data_sum = data.sum()
     m10 = raw_moment(data, 1, 0)
@@ -64,14 +39,68 @@ def parameters(data):
     x0 = m10 / data_sum
     y0 = m01 / data_sum
     u11 = (raw_moment(data, 1, 1) - x0 * m01) / data_sum
-    sx = (raw_moment(data, 2, 0) - x0 * m10) / data_sum
-    sy = (raw_moment(data, 0, 2) - y0 * m01) / data_sum
+    sx = np.sqrt((raw_moment(data, 2, 0) - x0 * m10) / data_sum)
+    sy = np.sqrt((raw_moment(data, 0, 2) - y0 * m01) / data_sum)
     theta = 0.5 * np.arctan(2 * u11 / (sx - sy))
 
-    return np.array([x0, y0, sx, sy, theta])
+    return bkg, height, x0, y0, sx, sy, theta
 
 
-def drift(data1, data2):
+def sim_gauss_est(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution by calculating its
+    moments """
+
+    height = np.max(data) - np.median(data)
+    bkg = np.median(data)
+
+    data = data - np.min(data)
+
+    total = data.sum()
+    X, Y = np.indices(data.shape)
+    x0 = (X*data).sum()/total
+    y0 = (Y*data).sum()/total
+    col = data[:, int(y0)]
+    sx = np.sqrt(abs((np.arange(col.size) - y0)**2*col).sum()/col.sum())
+    row = data[int(x0), :]
+    sy = np.sqrt(abs((np.arange(row.size) - x0)**2*row).sum()/row.sum())
+
+    return bkg, height, x0, y0, sx, sy
+
+
+def simmetric_gaussian(bkg, height, x0, y0, sx, sy):
+    """Returns a gaussian function with the given parameters"""
+    sx = float(sx)
+    sy = float(sy)
+    return lambda x, y: bkg + height*np.exp(-(((x0 - x)/sx)**2
+                                            + ((y0 - y)/sy)**2)/2)
+
+
+# Generic gaussian definition taken from
+# http://en.wikipedia.org/wiki/Gaussian_function
+def generic_gaussian(bkg, amp, x0, y0, sx, sy, theta):
+    x0 = float(x0)
+    y0 = float(y0)
+
+    a = np.cos(theta)**2 / (2 * sx**2) + np.sin(theta)**2 / (2 * sy**2)
+    b = - np.sin(2 * theta) / (4 * sx**2) + np.sin(2 * theta) / (4 * sy**2)
+    c = np.sin(theta)**2 / (2*sx**2) + np.cos(theta)**2 / (2 * sy**2)
+
+    return lambda x, y: bkg + amp * np.exp(- (a * (x - x0)**2 +
+                                           2 * b * (x - x0) * (y - y0) +
+                                           c * (y - y0)**2))
+
+
+def fit_LS(function, data, params):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution found by a fit"""
+    errorfunction = lambda p: np.ravel(function(*p)(*np.indices(data.shape)) -
+                                       data)
+    p, success = opt.leastsq(errorfunction, params)
+    return p
+
+
+def drift(data1, data2, i):
 
     data1 = data1 - np.median(data1)
     data2 = data2 - np.median(data2)
@@ -79,56 +108,53 @@ def drift(data1, data2):
     # Correlation calculation and cropping
     correlation = fftconvolve(data1, data2[::-1, ::-1], mode="same")
     imax = np.unravel_index(correlation.argmax(), correlation.shape)
-    crop_l = 5
+    crop_l = 10
     crop_corr = correlation[imax[0] - crop_l:imax[0] + crop_l + 1,
                             imax[1] - crop_l:imax[1] + crop_l + 1]
-#    crop_corr -= correlation.mean()
 
-    # Fitting
-    x = np.arange(2 * crop_l + 1)
-    y = np.arange(2 * crop_l + 1)
-    x, y = np.meshgrid(x, y)
-    guess = np.concatenate(([crop_corr.min(),
-                             crop_corr.max() - crop_corr.min()],
-                            parameters(crop_corr)))
-    popt, pcov = opt.curve_fit(generic_gaussian, (x, y),
-                               crop_corr.ravel(), p0=guess)
-#    guess = np.concatenate(([crop_corr.min(),
-#                             crop_corr.max() - crop_corr.min()],
-#                            parameters(crop_corr)[:3]))
-#    popt, pcov = opt.curve_fit(simmetric_gaussian, (x, y),
-#                               crop_corr.ravel(), p0=guess)
-
-    print('guess', guess)
-    print('popt', popt)
-
-    # Plots
-    data_fitted = generic_gaussian((x, y), *popt)
-#    data_fitted = simmetric_gaussian((x, y), *popt)
-    fig, ax = plt.subplots(1, 1)
-    ax.hold(True)
-    corr_plot = ax.imshow(crop_corr, cmap=plt.cm.jet, origin='bottom',
-                          extent=(x.min(), x.max(), y.min(), y.max()),
-                          interpolation='None')
-    fig.colorbar(corr_plot)
-    ax.contour(x, y, data_fitted.reshape(2 * crop_l + 1, 2 * crop_l), 8,
-               colors='w')
+    params_gen = fit_LS(generic_gaussian, crop_corr,
+                        gen_gauss_est(crop_corr))
+    params_sim = fit_LS(simmetric_gaussian, crop_corr,
+                        sim_gauss_est(crop_corr))
+    params_cm = center_of_mass(crop_corr - np.min(crop_corr))
 
     # Drift calculation
-    init = np.array(data1.shape) / 2
-    drift = [imax[0] - crop_l + popt[2], imax[1] - crop_l + popt[3]] - init
+    init = np.floor(np.array(data1.shape) / 2)
+    drift_gen = [imax[0] - crop_l + params_gen[2],
+                 imax[1] - crop_l + params_gen[3]] - init
+    drift_sim = [imax[0] - crop_l + params_sim[2],
+                 imax[1] - crop_l + params_sim[3]] - init
+    drift_cm = [imax[0] - crop_l + params_cm[0],
+                imax[1] - crop_l + params_cm[1]] - init
 
-    return drift[1], drift[0]
+    # Plots
+    if i == 1:
+        fit = generic_gaussian(*params_gen)
+        fig, ax = plt.subplots(1, 1)
+        ax.hold(True)
+        corr_plot = ax.imshow(crop_corr, cmap=plt.cm.jet, origin='bottom',
+                              interpolation='None')
+        fig.colorbar(corr_plot)
+        ax.contour(fit(*np.indices(crop_corr.shape)), cmap=plt.cm.copper)
+        print()
+        print(drift_gen)
+
+    return (drift_gen[0], drift_gen[1],
+            drift_sim[0], drift_sim[1],
+            drift_cm[0], drift_cm[1])
 
 
 def drift_track(data):
 
     n = len(data)
-    x, y = np.zeros(n), np.zeros(n)
+    x_gen, y_gen = np.zeros(n), np.zeros(n)
+    x_sim, y_sim = np.zeros(n), np.zeros(n)
+    x_cm, y_cm = np.zeros(n), np.zeros(n)
     for i in np.arange(1, n):
-        x[i], y[i] = drift(data[0], data[i])
+        (x_gen[i], y_gen[i],
+         x_sim[i], y_sim[i], x_cm[i], y_cm[i]) = drift(data[0], data[i], i)
 
-    return x, y
+    return x_gen, y_gen, x_sim, y_sim, x_cm, y_cm
 
 
 import matplotlib.pyplot as plt
@@ -167,7 +193,7 @@ paths = [folder + r for r in results]
 scale = 133/40
 
 nbins = np.ceil(np.array([253, 239]) * scale)
-#histos = np.array([make_histo(p, nbins) for p in paths])
+# histos = np.array([make_histo(p, nbins) for p in paths])
 
 xl, yl = get_i3_results(paths[0])
 for p in np.arange(1, len(paths)):
@@ -175,13 +201,15 @@ for p in np.arange(1, len(paths)):
     xl, yl = np.hstack((xl, xn)), np.hstack((yl, yn))
 
 n_locs = len(xl)
-xl, yl = chunker(xl, 110000), chunker(yl, 110000)
+xl, yl = chunker(xl, 100000), chunker(yl, 100000)
 histos = np.array([np.histogram2d(y, x, bins=nbins)[0]
                   for x, y in zip(xl, yl)])
 
-xt, yt = drift_track(histos)
-xt, yt = xt / scale, yt / scale
-print(xt, yt)
+tracks = drift_track(histos)
+xt, yt = tracks[0], tracks[1]
+print(xt[1], yt[1])
+#xt, yt = xt / scale, yt / scale
+#print(xt[1], yt[1])
 
 x_loc, y_loc = xl[0], yl[0]
 for p in np.arange(1, len(paths)):
@@ -216,92 +244,3 @@ ax11 = plt.subplot(gs[1, 1])
 img11 = ax11.imshow(H, interpolation='none', vmax=10)
 plt.xlabel('corrected')
 fig.colorbar(img11, ax=ax11)
-
-# LOCALIZATION BINNING
-#H, xedges, yedges = np.histogram2d(y_loc, x_loc, bins=nbins)
-#extent = [yedges[0], yedges[-1],  xedges[0], xedges[-1]]
-#
-## scipy.misc.imsave('out.png', H)
-#
-#plt.imshow(H, extent=extent, cmap='gray', vmax=10, interpolation='none')
-#plt.xlabel('$\mu$m')
-#plt.ylabel('$\mu$m')
-#plt.colorbar()
-
-
-def compare(path0, path1):
-
-    import matplotlib.gridspec as gridspec
-
-    nbins = np.ceil(np.array([253, 239]) * 133/40)
-    data0 = make_histo(path0, nbins)
-    data1 = make_histo(path1, nbins)
-
-
-    fig = plt.figure(figsize=(17.0, 7.0))
-    gs = gridspec.GridSpec(2, 2)
-
-    ax00 = plt.subplot(gs[0, 0])
-    img00 = ax00.imshow(data0, interpolation='none')
-    plt.xlabel('data0')
-    fig.colorbar(img00, ax=ax00)
-
-    ax01 = plt.subplot(gs[0, 1])
-    img01 = ax01.imshow(data1, interpolation='none')
-    plt.xlabel('data1')
-    fig.colorbar(img01, ax=ax01)
-
-    ax0 = plt.subplot(gs[1, 0])
-    summed = data0 + data1
-    img = ax0.imshow(summed, interpolation='none')
-    plt.xlabel('Summed')
-    fig.colorbar(img, ax=ax0)
-    ax0.axis('image')
-
-    # Correlation calculation and cropping
-    correlation = fftconvolve(data0, data1[::-1, ::-1], mode="same")
-    correlation = correlation - np.median(correlation)
-    imax = np.unravel_index(correlation.argmax(), correlation.shape)
-    crop_l = 12
-    crop_corr = correlation[imax[0] - crop_l:imax[0] + crop_l,
-                            imax[1] - crop_l:imax[1] + crop_l]
-    crop_corr -= correlation.mean()
-
-    # Fitting
-    x = np.arange(2 * crop_l)
-    y = np.arange(2 * crop_l)
-    x, y = np.meshgrid(x, y)
-    guess = np.concatenate(([crop_corr.min(),
-                             crop_corr.max() - crop_corr.min()],
-                            parameters(crop_corr)))
-    popt, pcov = opt.curve_fit(generic_gaussian, (x, y),
-                               crop_corr.ravel(), p0=guess)
-
-    # Drift calculation
-    init = np.array(data1.shape) / 2
-    drift = [imax[0] - crop_l + popt[1], imax[1] - crop_l + popt[2]] - init
-    dx, dy = drift[1]*40/133, drift[0]*40/133
-    print('dx, dy', drift)
-
-#    dx, dy = drift(data0, data1)
-    x0, y0 = get_i3_results(path0)
-    x1, y1 = get_i3_results(path1)
-    x_loc, y_loc = np.hstack((x0, x1 + dx)), np.hstack((y0, y1 + dy))
-
-    ax1 = plt.subplot(gs[1, 1])
-    hc = np.histogram2d(y_loc, x_loc, bins=nbins, normed=True)[0]
-    img1 = ax1.imshow(hc, interpolation='None')
-    fig.colorbar(img1, ax=ax1)
-    plt.xlabel('Corrected')
-    ax1.axis('image')
-
-    # Correlation plots
-    data_fitted = generic_gaussian((x, y), *popt)
-    fig, ax = plt.subplots(1, 1)
-    ax.hold(True)
-    corr_plot = ax.imshow(crop_corr, cmap=plt.cm.jet, origin='bottom',
-                          extent=(x.min(), x.max(), y.min(), y.max()),
-                          interpolation='None')
-    fig.colorbar(corr_plot)
-    ax.contour(x, y, data_fitted.reshape(2 * crop_l, 2 * crop_l), 8,
-               colors='w')
