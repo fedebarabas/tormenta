3f53ee8e61a4699fed64a7b03579425e2b725d5d
Federico Barabas <fede.barabas@gmail.com>
2015-04-28 15:22:40 -0300

    py2exe first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 15:15:33 2015
+
+@author: federico
+"""
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['/control/control.py'])
