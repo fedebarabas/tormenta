6e5a507a605f21671eb9293e5682a398309bef34
Federico Barabas <fede.barabas@gmail.com>
2015-07-17 20:36:11 -0300

    affine transformation

--- tormenta/analysis/tools.py
+++ tormenta/analysis/tools.py
@@ -7,6 +7,67 @@ Created on Tue Feb 18 18:03:01 2014
 import numpy as np
 
 
+def Haffine_from_points(fp, tp):
+    """ find H, affine transformation, such that tp is affine transf of fp.
+    Taken from http://www.janeriksolem.net/2009/06/
+    affine-transformations-and-warping.html
+    """
+
+    if fp.shape != tp.shape:
+        raise RuntimeError('number of points do not match')
+
+    # Condition points
+    # -from points-
+    m = mean(fp[:2], axis=1)
+    maxstd = max(std(fp[:2], axis=1))
+    C1 = diag([1/maxstd, 1/maxstd, 1])
+    C1[0][2] = -m[0]/maxstd
+    C1[1][2] = -m[1]/maxstd
+    fp_cond = dot(C1, fp)
+
+    # -to points-
+    m = mean(tp[:2], axis=1)
+    C2 = C1.copy()  # must use same scaling for both point sets
+    C2[0][2] = -m[0]/maxstd
+    C2[1][2] = -m[1]/maxstd
+    tp_cond = dot(C2, tp)
+
+    # conditioned points have mean zero, so translation is zero
+    A = concatenate((fp_cond[:2], tp_cond[:2]), axis=0)
+    U, S, V = linalg.svd(A.T)
+
+    # create B and C matrices as Hartley-Zisserman (2:nd ed) p 130.
+    tmp = V[:2].T
+    B = tmp[:2]
+    C = tmp[2:4]
+
+    tmp2 = concatenate((dot(C, linalg.pinv(B)), zeros((2, 1))), axis=1)
+    H = vstack((tmp2, [0, 0, 1]))
+
+    # decondition
+    H = dot(linalg.inv(C2), dot(H, C1))
+
+    return H / H[2][2]
+
+#transformed_im = ndimage.affine_transform(im,A,b,size)
+#
+#transforms the image patch im with A a linear transformation and b a translation vector as above. The optional argument size can be used to specify the size of the output image. The default is an image with the same size as the original. To see how this works, try running the following commands:
+#
+#>>>from PIL import Image
+#>>>from scipy import ndimage
+#>>>from pylab import *
+#
+#>>>im = array(Image.open('empire.jpg').convert('L'))
+#>>>H = array([[1.4,0.05,-100],[0.05,1.5,-100],[0,0,1]])
+#>>>im2 = ndimage.affine_transform(im,H[:2,:2],(H[0,2],H[1,2]))
+#
+#>>>figure()
+#>>>gray()
+#>>>imshow(im2)
+#>>>show()
+#https://stackoverflow.com/questions/27546081/determining-a-homogeneous-affine-transformation-matrix-from-six-points-in-3d-usi/27547597#27547597
+#http://elonen.iki.fi/code/misc-notes/affine-fit/
+
 def mode(array):
     hist, bin_edges = np.histogram(array, bins=array.max() - array.min())
     hist_max = hist.argmax()
